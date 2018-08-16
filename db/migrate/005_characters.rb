5b5c767bbb2867397151a539674167bf221837d2
Wasif Khan <waasif.khaan@gmail.com>
2018-08-16 16:40:34 +0000

    automatically backed up by learn

--- db/migrate/005_characters.rb
+++ db/migrate/005_characters.rb
@@ -3,7 +3,7 @@ class Chacracters < ActiveRecord::Migration[4.2]
     create_table :characters do |t|
       #primary key of :id is created for us!
       t.string :name
-      t.string :show_id
+      t.integer :show_id
     end
   end 
 end
