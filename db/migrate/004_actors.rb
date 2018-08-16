8fc86ee0fccc2d721c6da16628be857fb002bc58
Wasif Khan <waasif.khaan@gmail.com>
2018-08-16 16:38:01 +0000

    automatically backed up by learn

--- db/migrate/004_actors.rb
+++ db/migrate/004_actors.rb
@@ -1,3 +1,9 @@
 class Actors < ActiveRecord::Migration[4.2]
   def change 
-    add_column :actors
+    def change
+    create_table :actors do |t|
+      #primary key of :id is created for us!
+      t.string :first_name
+      t.string :last_name
+    end
+  end
